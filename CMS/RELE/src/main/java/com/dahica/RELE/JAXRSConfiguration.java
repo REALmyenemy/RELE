695ef92649f10580aa8c3a615b527ea39467f3f3
REALmyenemy <26635506+REALmyenemy@users.noreply.github.com>
2021-04-03 00:24:29 +0200

    Initial commit

---
+++ CMS/RELE/src/main/java/com/dahica/RELE/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package com.dahica.RELE;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
